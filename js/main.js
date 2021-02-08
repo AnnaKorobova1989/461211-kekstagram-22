22e1992127010f46a1904793911310af2f3aef12
Anna <korobovaanna89@gmail.com>
2021-02-08 21:47:47 +0300

    Правки в функциях с учетом замечаний

--- js/main.js
+++ js/main.js
@@ -2,26 +2,19 @@ let anyString = 'djdhgjhfdg';
 let stringLength = 10;
 
 let getStrMaxLength = function (str, strMaxLength) {
-  if (str.length <= strMaxLength) {
-    return true;
-  } else {
-    return false;
-  }
+  return str.length <= strMaxLength;
 }
 
 getStrMaxLength(anyString, stringLength);
 
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   if (min >= max) {
     alert('Задан неверный диапазон');
-  } else {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return;
   }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
   
-const minInt = 0;
-const maxInt = 10;
-  
-getRandomIntInclusive(minInt, maxInt);
+getRandomIntInclusive(0, 10);
