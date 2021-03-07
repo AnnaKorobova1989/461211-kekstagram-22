f4ae7fd26b49d4c912bc57b9b81e3195592f59d2
Anna <korobovaanna89@gmail.com>
2021-03-07 21:57:46 +0300

    С моими данными

--- js/big-photo.js
+++ js/big-photo.js
@@ -1,8 +1,117 @@
-import {getComments, getPhotoDescriptions} from './data.js';
-import {getRandomIntInclusive} from './util.js';
-import {similarPhotoElement} from './render-photos.js';
+const socialCaption = document.querySelector('.social__caption');
+const socialCommentCount = document.querySelector('.social__comment-count');
+const commentsList = document.querySelector('.social__comments');
+const templateComment = document.querySelector('#social__comment').content;
 
+const bigPictureImg = document.querySelector('.big-picture__img > img');
 const bigPicture = document.querySelector('.big-picture');
+const closeBtn = bigPicture.querySelector('.big-picture__cancel');
+
+const likesCount = document.querySelector('.likes-count');
+const commentsCount = document.querySelector('.comments-count');
+const body = document.querySelector('body');
+const commentsLoader = document.querySelector('.comments-loader');
+
+
+
+//обработчик клика по клаве
+const onEscapeKayDown = function (evt) {
+  if (evt.key === ('Escape' || 'Esc')) {
+    closeModal();
+  }
+}
+
+const openModal = function() {
+  bigPicture.classList.remove('hidden');
+  socialCommentCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+  body.classList.add('modal-open');
+
+  closeBtn.addEventListener('click', closeModal);
+  document.addEventListener('keydown', onEscapeKayDown);
+};
+
+const closeModal = function() {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+
+  closeBtn.removeEventListener('click', closeModal);
+  document.removeEventListener('click', onEscapeKayDown);
+};
+
+//начальная функция, чтобы наполнить шаблон данными
+export const createBigPhoto = function({url, likes, description, comments}) {
+  bigPictureImg.src = url;
+  likesCount.textContent = likes;
+  socialCaption.textContent = description;
+  commentsCount.textContent = createComments.length;
+  
+  fillComments(comments);
+  openModal();
+}
+
+const fillComments = function (comments) {
+  commentsList.textContent = ''; //очищаем содержимое
+  commentsList.appendChild(createComments(comments));//наполняем картинкой и текстом
+};
+
+const createComments = function (comments) {
+  const fragment = document.createDocumentFragment();
+
+  comments.forEach((comment) => {
+    fragment.appendChild(createComment(comment));
+  })
+
+  return fragment;
+};
+
+const createComment = function (comment) {
+  //найти разметку для комментария
+  const element = templateComment.querySelector('.social__comment').cloneNode(true);
+
+  //в разметке найти необходимые элементы
+  const socialImg = element.querySelector('.social__picture');
+  const socialText = element.querySelector('.social__text');
+
+  //наполнить эти элементы необходимыми данными
+  socialImg.setAttribute('src', comment.avatar);
+  socialImg.setAttribute('alt', comment.name);
+  socialText.textContent = comment.message;
+  //вернуть элумент с наполненными данными
+
+  return element;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import {getComments, getPhotoDescriptions} from './data.js';
+import {getRandomIntInclusive} from './util.js';
+import {similarPhotoElement} from './render-photos.js';*/
+
+/*const bigPicture = document.querySelector('.big-picture');
 const closeBtn = bigPicture.querySelector('.big-picture__cancel');
 const commentsList = document.querySelector('.social__comments');
 const avatarWidth = 35;
@@ -37,33 +146,51 @@ document.addEventListener('keydown', function (evt) {
   }
 });
 
-const makeElement = function(tagName, className, text) {
-  var element = document.createElement(tagName);
-  element.classList.add(className);
-  if (text) {
-    element.textContent = text;
-  }
-  return element;
-};
-
-const createComments = function(comment) {
-  const newComment = makeElement('li', 'social__comment');
+const createTemplate = function() {
+  var commentTemplate = document.createElement('template');
+  commentTemplate.id = 'social__comment';
+  body.appendChild(commentTemplate);
+  var newComment = document.createElement('li');
+  newComment.classList.add('social__comment');
   commentsList.appendChild(newComment);
-
-  const newAvatar = makeElement('img', 'social__picture');
-  newAvatar.src = comment.avatar;
-  newAvatar.alt = comment.names;
+  commentTemplate.appendChild(newComment);
+  const newAvatar = document.createElement('img');
+  newAvatar.classList.add('social__picture');
+  newAvatar.src;
+  newAvatar.alt;
   newAvatar.width = avatarWidth;
   newAvatar.height = avatarHeight;
   newComment.appendChild(newAvatar);
-
-  const socialText = makeElement('p', 'social__text', comment.message);
+  const socialText = document.createElement('p');
+  socialText.classList.add('social__text');
   newComment.appendChild(socialText);
-  return newComment;
+  return commentTemplate;
 }
 
-var commentItem = createComments(arrayOfComments);
-commentsList.appendChild(commentItem);
+var commentItem = createTemplate();
+
+const similarPhotoComments = document.querySelector('.social__comment');
+console.log(similarPhotoComments);
+const templateComment = document.querySelector('#social__comment').content.querySelector('.social__comment');
+const similarComments = getComments(getRandomIntInclusive(3, 7));
+const similarCommentFragment = document.createDocumentFragment();
+
+console.log(templateComment);
+
+const drawComments = () => {
+  similarComments.forEach(({avatar, message}) => {
+    const commentElement = templateComment.cloneNode(true);
+    commentElement.querySelector('img').src = avatar;
+    commentElement.querySelector('.social__text').textContent = message;
+    similarCommentFragment.appendChild(commentElement);
+    return similarCommentFragment;
+  })
+}
+
+drawComments();
+similarPhotoComments.appendChild(similarCommentFragment);
+
+/*commentsList.appendChild(commentItem);
 
 for (var i = 0; i < arrayOfComments.length; i++) {
   var cardItem = createComments(arrayOfComments[i]);
@@ -79,7 +206,7 @@ const createBigPhoto = function({url, likes, description}) {
 
 for (var i = 0; i < arrayOfPhotoDescription.length; i++) {
     var descriptionPhoto = createBigPhoto(arrayOfPhotoDescription[i]);
-}
+}*/
 
 
 
--- js/render-photos.js
+++ js/render-photos.js
@@ -1,4 +1,5 @@
 import {getPhotoDescriptions} from './data.js';
+import {createBigPhoto} from './big-photo.js';
 export {similarPhotoElement};
 
 //import { getRandomIntInclusive } from './util.js';
@@ -6,19 +7,30 @@ export {similarPhotoElement};
 const similarPhotoElement = document.querySelector('.pictures');
 const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
 const similarPhotos = getPhotoDescriptions(25);
-const similarPhotoFragment = document.createDocumentFragment();
 
 const drawPhotos = () => {
-  similarPhotos.forEach(({url, comments, likes}) => {
+  const similarPhotoFragment = document.createDocumentFragment();
+
+  similarPhotos.forEach((data) => {
+    const {url, comments, likes} = data;
     const photoElement = templatePicture.cloneNode(true);
+
     photoElement.querySelector('img').src = url;
     photoElement.querySelector('.picture__comments').textContent = comments.length;
     photoElement.querySelector('.picture__likes').textContent = likes;
+
+    photoElement.addEventListener('click', function(evt) {
+      evt.preventDefault();
+      createBigPhoto(data);
+    })
+
     similarPhotoFragment.appendChild(photoElement);
     return similarPhotoFragment;
   })
+
+  similarPhotoElement.appendChild(similarPhotoFragment);
 }
 
 drawPhotos();
 
-similarPhotoElement.appendChild(similarPhotoFragment);
+
